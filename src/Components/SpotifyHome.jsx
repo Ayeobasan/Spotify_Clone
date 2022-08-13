e110e60332175fd866196649bba11f3771c664c3
miriam-samuels <medomiriam@gmail.com>
2022-08-13 13:18:24 +0100

    Began refactoring- fixed home responsiveness issue

--- src/Components/SpotifyHome.jsx
+++ src/Components/SpotifyHome.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import '../Styles/Spotify.css'
 import spotify from '../img/spotify.png';
-import { FaMusic, FaPlay, FaSpotify } from 'react-icons/fa';
-import Spotify from '../img/spotify.png'
-import {NavLink} from 'react-router-dom'
+import { FaMusic, FaPlay } from 'react-icons/fa';
+import { Link } from 'react-router-dom'
 
 function SpotifyHome() {
-    return <div className='SpotifyContainer'>
-        <div className="music">
-            <img src={spotify} alt="" />
-            <div className="spotText">
-                <p>Listen to
-                    <sup>
-                        <FaMusic />
-                        <FaMusic />
-                    </sup>
-                </p>
-                <h1>Spotify</h1>
+    return (
+        <div className='SpotifyContainer'>
+            <div>
+                <div className="music">
+                    <img src={spotify} alt="" />
+                    <div className="spotText">
+                        <p>Listen to
+                            <sup>
+                                <FaMusic />
+                                <FaMusic />
+                            </sup>
+                        </p>
+                        <h1>Spotify</h1>
+                    </div>
+                </div>
+                <Link to='/Home'>
+                    <button><FaPlay /></button>
+                </Link>
             </div>
-            <a href='/Home'> 
-                <button><FaPlay /></button>
-            </a>
         </div>
-
-    </div>
-
-
+    )
 }
 
 export { SpotifyHome }
